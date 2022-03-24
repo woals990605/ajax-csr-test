5a8a736d3426005bc3bc2590b29b9d7f6de87a4c
woals990605 <woals990605@naver.com>
2022-03-24 12:35:53 +0900

    6.ajax로 csr하기-with jquery

--- src/main/java/site/metacoding/dbtest/web/BoardTblController.java
+++ src/main/java/site/metacoding/dbtest/web/BoardTblController.java
@@ -1,28 +1,13 @@
 package site.metacoding.dbtest.web;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import site.metacoding.dbtest.domain.boardTbl.BoardTbl;
-import site.metacoding.dbtest.domain.boardTbl.BoardTblRepository;
 
 @Controller
 public class BoardTblController {
 
-    private BoardTblRepository boardTblRepository;
-
-    public BoardTblController(BoardTblRepository boardTblRepository) {
-        this.boardTblRepository = boardTblRepository;
-    }
-
-    @GetMapping("/search")
-    public String search(Model model) {
-
-        List<BoardTbl> boards = boardTblRepository.findAll();
-        model.addAttribute("boards", boards);
+    @GetMapping("/")
+    public String home() {
         return "home";
     }
 }
